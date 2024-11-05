270fddd57196e4d1e6b76f9713b44446536ca49c
elijah-lewis5 <eqlewis2@illinois.edu>
2024-11-05 17:18:37 -0600

    New exp generator optimized for 1 parameter

---
+++ Component Analysis/Scripts/generate exp files v2.py
@@ -0,0 +1,53 @@
+#generate exp files
+#This will delete all files currently in Documents/LRI CFD/NX/EXP
+#Step 1: Open expressions table and find the name and unit of the parameters to be changed. 
+#Step 2: Type in unit type exactly (MilliMeter,Meter,Feet,Inch,Degrees) for each parameter
+
+#Step 3: Type desired decimal precision
+precision = 3 #number of decimals
+
+#Step 4: Fill out parameters and steps
+#Min value, max value (inclusive) and step size 
+parameter = 'angle1'
+min = 0
+max = 30
+step = 1/3
+units = 'Degrees'   #MilliMeter,Meter,Feet,Inch,Degrees
+#Step 5: Run code
+
+
+import os
+def delete_files_in_folder(folder_path):
+    #Delete all files in the specified folder.
+    #:param folder_path: The path to the folder from which files will be deleted.
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):     # Check if the folder exists
+        # Iterate over all the files in the folder
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            # Check if it's a file (not a subfolder)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+    else:
+        print(f"The folder {folder_path} does not exist or is not a directory.")
+
+#User's folder path generated from folder structure script        
+user_home = os.path.expanduser("~")
+path = os.path.join(user_home, "Documents","LRI CFD","NX","EXPS","")
+delete_files_in_folder(path)
+
+def float_range(start,stop,step):
+    #equivalent to numpy.arange(start,stop+step,step)
+    #inclusive of both start and stop
+    while start<stop+step:
+        yield round(start,precision)
+        start += step
+
+count = 0
+for i in float_range(min,max,step):
+    count += 1
+    outputpath = f'{path}{parameter}_{i}'
+    text = f"// Version:  3\n[{units}]{parameter}={i}"
+    with open(f"{outputpath}.exp",'w') as f:
+            f.write(text)
+            f.close()
+print(f"Succesfully created {count} files in {path}")
